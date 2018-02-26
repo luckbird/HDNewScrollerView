c57f174f50561c7cb78cb0e7081619b00eec85ca
yscompany <814880548@qq.com>
2018-02-26 17:38:06 +0800

    First commit

---
+++ HDNewScrollerViewExample/HDNewScrollerView/HDNewScrollerView/HDNewScrollerView.h
@@ -0,0 +1,13 @@
+//
+//  HDNewScrollerView.h
+//  HDNewScrollerView
+//
+//  Created by yscompany on 2018/2/26.
+//  Copyright © 2018年 yscompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDNewScrollerView : UIScrollView
+
+@end
---
+++ HDNewScrollerViewExample/HDNewScrollerView/HDNewScrollerView/HDNewScrollerView.m
@@ -0,0 +1,21 @@
+//
+//  HDNewScrollerView.m
+//  HDNewScrollerView
+//
+//  Created by yscompany on 2018/2/26.
+//  Copyright © 2018年 yscompany. All rights reserved.
+//
+
+#import "HDNewScrollerView.h"
+
+@implementation HDNewScrollerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
